79b092fcf0bcb28f51a2e6ef8091b5768a6a2f48
bongholee <leebongho9204@gmail.com>
2023-01-09 22:52:14 +0900

    Lecture 1

---
+++ src/main/java/com/basic/kotlin/Lecture1_Java.java
@@ -0,0 +1,10 @@
+package com.basic.kotlin;
+
+public class Lecture1_Java {
+    public static void main(String[] args) {
+        long number1 = 10L;
+        final long number2 = 10L;
+
+
+    }
+}
