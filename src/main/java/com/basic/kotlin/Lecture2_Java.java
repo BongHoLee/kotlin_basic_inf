8c0aaf8bc3942ee4cdf60f356887a5d00bae387a
bongholee <leebongho9204@gmail.com>
2023-02-01 07:33:39 +0900

    섹션1 종료

--- src/main/java/com/basic/kotlin/Lecture2_Java.java
+++ src/main/java/com/basic/kotlin/Lecture2_Java.java
@@ -1,4 +1,13 @@
 package com.basic.kotlin;
 
 public class Lecture2_Java {
+    public static void main(String[] args) {
+        Person person = new Person("lee");
+        String log = String.format("사람의 이름은 %s 입니다.", person.getName());
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("사람의 이름은");
+        builder.append(person.getName());
+        builder.append("입니다.");
+    }
 }
--- src/main/kotlin/com/basic/kotlin/Lecture2_Kotlin.kt
+++ src/main/kotlin/com/basic/kotlin/Lecture2_Kotlin.kt
@@ -1,4 +1,24 @@
 package com.basic.kotlin
 
-class Lecture2_Kotlin {
+fun main() {
+    val number1 = 3
+    val number2: Long = 3 + number1.toLong()
+
+    val n1: Int? = 3
+    val n2: Long = n1?.toLong() ?: 0L
+}
+
+fun printAgeIfPerson(obj: Any) {
+    if (obj is Person) {
+        println(obj.name)
+    }
+}
+
+fun printAgeIfPerson2(obj: Any?) {
+    val person = obj as? Person
+    println(person?.name)
+}
+
+fun interpolation() {
+
 }
